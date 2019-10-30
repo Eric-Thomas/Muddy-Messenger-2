57c5ebf752e5b1f92ff170b7cbacdd69b202a4f4
Eric Thomas <bluedevils027@gmail.com>
2019-10-30 19:20:51 -0400

    Added flask and flask-cors. Can write to db w sqlalchemy via backend

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,18 @@
-from database import init_db
-init_db()
+import database
+from models import User
+from flask import Flask
+from flask_cors import CORS
+
+
+database.init_db()
+# This line is only to test that adding to the database works correctly
+admin = User('admin')
+db_session = database.db_session
+db_session.add(admin)
+db_session.commit()
+
+app = Flask(__name__)
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,12 @@
 from sqlalchemy import Column, String
-from .. import database
+from database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(50), unique=True)
+    user_name = Column(String(50), unique=True, primary_key=True)
     public_key = Column(String(120))
 
-    def __init__(self, name=None, email=None):
-        self.username = name
+    def __init__(self, name=None):
+        self.user_name = name
         #TODO: replace this with DH or RSA public key generation
         self.public_key = '00112233445566778899aabbccddeeff'
