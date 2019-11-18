cc60f1ece6dcf4967f8d699b3eca9b566279b6dd
Eric Thomas <bluedevils027@gmail.com>
2019-11-18 15:33:06 -0500

    updated requirements and working on querying db for users

--- backend/main.py
+++ backend/main.py
@@ -11,22 +11,29 @@ db_session = database.db_session
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/user', methods=['POST'])
-def add_user():
-    user_name = request.get_json().get('user_name')
-    if user_name != None:
+@app.route('/user', methods=['POST', 'GET'])
+def user():
+    request_user_name = request.get_json().get('user_name')
+    if request_user_name == None: # check that username isn't emtpy
+        return jsonify({'status': 400, 'message': 'Invalid request syntax. Send JSON of form {user_name: XXX}'})
+    
+    if request.method == 'POST': # Add user to db
         try:
             #TODO: Sanitize DB inputs
-            user = User(user_name)
+            user = User(request_user_name)
             db_session.add(user)
             db_session.commit()
-            # Successful right to database
-            return jsonify({'status': 201, 'message': str(user_name) + ' added'})
+            # Successful write to database
+            return jsonify({'status': 201, 'message': str(request_user_name) + ' added'})
         except exc.IntegrityError as e:
             db_session().rollback()
             return jsonify({'status': 400, 'message': 'User already exists in db. Usernames must be unique'})
-    else:
-        return jsonify({'status': 400, 'message': 'Invalid request syntax. Send JSON of form {user_name: XXX}'})
+    else: # GET method return query of users
+        q = db_session.query(User).filter_by(user_name = request_user_name).all()
+        for row in q:
+            print("user name: " + row.user_name + "\nPublic key: " + row.public_key)
+        return jsonify({'status': 200})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
