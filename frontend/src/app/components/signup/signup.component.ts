c83c6e8246ec9f0af97eb65ac2e1e02cfc4efda2
Michael Huang <huang.2721@osu.edu>
2019-11-18 22:14:38 -0500

    merged authentication to master

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
