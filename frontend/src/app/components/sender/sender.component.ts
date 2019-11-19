1105284206a2e5723e4240537d5f40bbdc342981
Eric-Thomas <bluedevils027@gmail.com>
2019-11-18 21:33:33 -0500

    removed old message encryption methods

--- frontend/src/app/components/sender/sender.component.ts
+++ frontend/src/app/components/sender/sender.component.ts
@@ -1,42 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sender',
-  templateUrl: './sender.component.html',
-  styleUrls: ['./sender.component.css']
+  selector: "app-sender",
+  templateUrl: "./sender.component.html",
+  styleUrls: ["./sender.component.css"]
 })
 export class SenderComponent implements OnInit {
-
-  private userName = '';
-  private encryption = '';
-  private submit = false;
-  private encryptionError = false;
-  private successMessage = '';
-  constructor(private userService: UserService,
-    private router: Router) { }
+  private userName = "";
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     this.userName = this.userService.getUserName();
     if (!this.userName) {
-      this.router.navigateByUrl('');
+      this.router.navigateByUrl("");
     }
   }
 
-  setEncryptionOption(option: string){
-    this.encryption = option;
-    this.encryptionError = false;
+  sendMessage() {
+    //TODO: Encrypt and send to backend
   }
-
-  sendMessage(){
-    //TODO: Send message with api service and use httpinterceptor to encrypt
-    if (!this.encryption){
-      this.encryptionError = true;
-    } else {
-      (<HTMLTextAreaElement>document.getElementById('message')).value = '';
-      this.successMessage = 'Message sent';
-    }
-  }
-
 }
