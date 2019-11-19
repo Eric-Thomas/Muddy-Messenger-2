f76cb8b85cf64f3cdd70d354d2c96d5b280c744a
Eric-Thomas <bluedevils027@gmail.com>
2019-11-18 22:16:08 -0500

    Changing GET users to /user/user_name

--- backend/main.py
+++ backend/main.py
@@ -17,25 +17,25 @@ def user():
     request_user_name = request.get_json().get('user_name')
     if not request_user_name:  # check that username isn't emtpy
         return jsonify({'status': 400, 'message': 'Invalid request syntax. Send JSON of form {user_name: XXX}'})
+    try:
+        # TODO: Sanitize DB inputs
+        user = User(request_user_name)
+        db_session.add(user)
+        db_session.commit()
+        # Successful write to database
+        return jsonify({'status': 201, 'message': str(request_user_name) + ' added'})
+    except exc.IntegrityError as e:
+        db_session().rollback()
+        return jsonify({'status': 400, 'message': 'User already exists in db. Usernames must be unique'})
 
-    if request.method == 'POST':  # Add user to db
-        try:
-            # TODO: Sanitize DB inputs
-            user = User(request_user_name)
-            db_session.add(user)
-            db_session.commit()
-            # Successful write to database
-            return jsonify({'status': 201, 'message': str(request_user_name) + ' added'})
-        except exc.IntegrityError as e:
-            db_session().rollback()
-            return jsonify({'status': 400, 'message': 'User already exists in db. Usernames must be unique'})
-    else:  # GET method return query of users
-        # add like characters to get usernames with input as a substring
-        like_name = "%{}%".format(request_user_name)
-        query = User.query.filter(User.user_name.like(like_name)).all()
-        if not query:  # No users match in db
-            return jsonify({'status': 404, 'message':  str(request_user_name) + ' does not exist'})
-        return jsonify({'status': 200, 'users': [row.user_name for row in query]})
+
+@app.route('/user/<user_name>')
+def user_name():
+    like_name = "%{}%".format(username)
+    query = User.query.filter(User.user_name.like(like_name)).all()
+    if not query:  # No users match in db
+        return jsonify({'status': 404, 'message':  str(user_name) + ' does not exist'})
+    return jsonify({'status': 200, 'users': [row.user_name for row in query]})
 
 
 if __name__ == '__main__':
