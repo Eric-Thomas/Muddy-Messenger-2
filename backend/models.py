edfdac4abb1a0e845170d0086123201e03b62ec0
Eric Thomas <bluedevils027@gmail.com>
2019-11-20 16:47:55 -0500

    Added password to User db

--- backend/models.py
+++ backend/models.py
@@ -2,11 +2,14 @@ from sqlalchemy import Column, String
 from database import Base
 
 class User(Base):
-    __tablename__ = 'users'
-    user_name = Column(String(50), unique=True, primary_key=True)
-    public_key = Column(String(120))
 
-    def __init__(self, name=None):
+    __tablename__ = 'users'
+    user_name = Column(String(50), unique=True, primary_key=True, nullable = False)
+    password = Column(String(50), nullable = False)
+    public_key = Column(String(120), nullable = False)
+    
+    def __init__(self, name = None, password = None):
         self.user_name = name
+        self.password = password
         #TODO: replace this with DH or RSA public key generation
         self.public_key = '00112233445566778899aabbccddeeff'
