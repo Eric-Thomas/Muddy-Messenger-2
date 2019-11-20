ba1b584883d17f58b7763d4f8ecc71cc18067470
Michael Huang <huang.2721@osu.edu>
2019-11-20 17:31:49 -0500

    fixed error in signup

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -37,7 +37,7 @@ export class SignupComponent implements OnInit {
       return;
     }
 
-    this.apiServiece.createUser(this.f.username.value);
+    this.apiServiece.createUser(this.f.username.value, this.f.password.value);
     this.userService.createUser(this.f.username.value);
     this.router.navigateByUrl('/inbox'); 
   }
