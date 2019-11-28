b25170c25dcf35e160e8b59d17c9eea2b71845b6
Michael Huang <huang.2721@osu.edu>
2019-11-27 21:43:08 -0500

    hash hash hash

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -3,12 +3,13 @@ import { AppConstants } from '../app.constants';
 import { HttpClient } from '@angular/common/http';
 import {throwError} from 'rxjs';
 import { map } from 'rxjs/operators';
+import * as cryptojs from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  
+  saltRounds = 10;
 
   constructor(private httpClient: HttpClient) {
    }
@@ -17,7 +18,7 @@ export class ApiService {
     let url = AppConstants.apiURL + "/authenticate/" + username;
     return this.httpClient.get(url).pipe(map(resp => {
       console.log("db pass" + resp["password"])
-      if (resp["status"] == 200 && resp["password"] == password){//successful login
+      if (resp["status"] == 200 && resp["password"] == this.hash(password, username)){//successful login
         return resp;
       }
       return this.error(resp["message"]);
@@ -25,10 +26,11 @@ export class ApiService {
   }
 
   createUser(username: String, password: String){
+    
     let url = AppConstants.apiURL + "/user";
     let payload = {
       "user_name": username,
-      "password": password
+      "password": this.hash(password, username)
     }
     return this.httpClient.post(url, payload)
     .pipe(map(resp => {
@@ -40,7 +42,11 @@ export class ApiService {
     }));
   }
 
-  error(message) {
+  error(message : String) {
     return throwError(message);
+  }
+
+  hash(plaintext : any, salt: any) {
+    return cryptojs.MD5(plaintext + salt).toString();
   }
 }
