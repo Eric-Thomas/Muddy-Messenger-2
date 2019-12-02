0dee27f47d7a6d32e6473e463c7893d07262961a
Michael Huang <huang.2721@osu.edu>
2019-12-01 20:05:58 -0500

    fixed errors

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -101,7 +101,7 @@ export class ApiService {
     }
   }
 
-  decryptMessage(plaintext: any, algorithm : string, sharedKey : string) {
+  decryptMessage(plaintext: any, algorithm : string, sharedKey : any) {
     //TODO: Produce key from user input
     switch(algorithm){
       case 'RSA': {
